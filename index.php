88d1a6c89812d17b0bf63bb18c93b15ea2a272fa
Stefano Tosatto <stefanotosatto.work@gmail.com>
2021-04-22 16:12:45 +0200

    ok

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP badwords</title>
+</head>
+<body>
+
+<h1>Badwords</h1>
+
+<?php
+    $string = 'Li vuoi quei kiwi? E se non vuoi quei kiwi che kiwi vuoi?';
+    $newString = str_replace('kiwi', 'xxxx', $string);
+?>
+<h3><?php echo $string ?></h3>
+
+<h3><?php echo $newString ?></h3>
+</body>
+</html>
